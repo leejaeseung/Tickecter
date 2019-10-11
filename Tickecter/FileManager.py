2026bd5f352555cd7c27291f54806c664cb945d2
choyj0920 <choyj0920@naver.com>
2019-10-11 10:04:09 +0900

    movielist update, filemanager- savefile

--- Tickecter/FileManager.py
+++ Tickecter/FileManager.py
@@ -8,22 +8,49 @@ class FileManager:
         ML = pd.read_csv("../MovieList.csv")
         RL = pd.read_csv("../ReservationList.csv")
 
-        # 카드리스트 dir으로
+        # 카드리스트 카드번호를 키로 딕셔너리
         self.cardlist = dict([(a, b) for a,b in zip(CL.cardnum, CL.regist)])
 
-        #유저리스트 아이디를 키로 dir
-        self.userlist = dict([(userID,{"password":password,"registcard":cardnum,"mileage":mileage})
+        #유저리스트 아이디를 키로 딕셔너리
+        self.userlist = dict([(userID,{"userpassword":password,"registcard":cardnum,"mileage":mileage})
                          for userID, password, cardnum, mileage in zip(UL.userID, UL.userpassword, UL.registcard,UL.mileage)])
 
         #영화일정리스트 이차원 리스트
-        self.movielist = [(day,moviecode,moviename,starttime,finishtime,screen,seat,[A,B,C,D,E,F,G,H,I,J])
+        self.movielist = [(day,moviecode,moviename,starttime,finishtime,screen,seat,str(A),B,C,D,E,F,G,H,I,J)
                           for day, moviecode,moviename,starttime,finishtime,screen,seat,A,B,C,D,E,F,G,H,I,J
-                          in zip(ML.day,ML.moviecode,ML.moviename,ML.starttime,ML.finishtime,ML.screen,ML.seat,ML.A,ML.B,ML.C,ML.D,ML.E,ML.F,ML.G,ML.H,ML.I,ML.J)]
+                          in zip(ML.day,ML.moviecode,ML.moviename,ML.starttime,ML.finishtime,ML.screen,ML.seat,str(ML.A),ML.B,ML.C,ML.D,ML.E,ML.F,ML.G,ML.H,ML.I,ML.J)]
+        #예약리스트 이차원 리스트
         self.reservationlist = [(member,userID,reservationcode,seats,cancel)
                                 for member,userID,reservationcode,seats,cancel in zip(RL.member,RL.userID,RL.reservationcode,RL.seats,RL.cancel)]
+    def savefile(self):
+        ##card
+        df_cardlist = pd.DataFrame()
+        df_cardlist['cardnum']= self.cardlist.keys()
+        df_cardlist['regist']= self.cardlist.values()
+
+        ##user
+        us_list = []
+        for a,b in self.userlist.items():
+            us_list.append((a,b["userpassword"],b["registcard"],b["mileage"]))
+        df_userlist = pd.DataFrame(us_list)
+
+        #movie
+        df_movielist= pd.DataFrame(self.movielist)
+
+        df_cardlist.to_csv("cardlist.csv",header=True,index=False)
+        df_userlist.set_axis(["userID", "userpassword", "registcard", "mileage"], axis='columns', inplace=True)
+        df_userlist.to_csv("userlist.csv",header=True,index=False)
+        df_movielist.set_axis(["day","moviecode",'moviename','starttime','finishtime','screen','seat','A','B','C','D','E','F','G','H','I','j'],axis='columns',inplace=True)
+        df_movielist.to_csv("movielist.csv",header=True,index=False)
 
 
-
-x = FileManager()
-print(x.reservationlist)
-print(x.movielist)
+x =FileManager()
+x.savefile()
+#사용예
+if 2<1:
+    #클래스 생성
+    x = FileManager()
+    print(x.userlist["u8s0e9r"]["password"])
+    print(x.movielist[3][2])
+    print(x.reservationlist)
+    print(x.movielist)
