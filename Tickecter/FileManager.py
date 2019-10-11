958544e03c8f10f8dad309bce67d9559117e6ded
choyj0920 <choyj0920@naver.com>
2019-10-11 10:36:34 +0900

    movielist update, 좌석 []로 묶어 생략안되게

--- Tickecter/FileManager.py
+++ Tickecter/FileManager.py
@@ -16,12 +16,13 @@ class FileManager:
                          for userID, password, cardnum, mileage in zip(UL.userID, UL.userpassword, UL.registcard,UL.mileage)])
 
         #영화일정리스트 이차원 리스트
-        self.movielist = [(day,moviecode,moviename,starttime,finishtime,screen,seat,str(A),B,C,D,E,F,G,H,I,J)
+        self.movielist = [(day,moviecode,moviename,starttime,finishtime,screen,seat,A,B,C,D,E,F,G,H,I,J)
                           for day, moviecode,moviename,starttime,finishtime,screen,seat,A,B,C,D,E,F,G,H,I,J
-                          in zip(ML.day,ML.moviecode,ML.moviename,ML.starttime,ML.finishtime,ML.screen,ML.seat,str(ML.A),ML.B,ML.C,ML.D,ML.E,ML.F,ML.G,ML.H,ML.I,ML.J)]
+                          in zip(ML.day,ML.moviecode,ML.moviename,ML.starttime,ML.finishtime,ML.screen,ML.seat,ML.A,ML.B,ML.C,ML.D,ML.E,ML.F,ML.G,ML.H,ML.I,ML.J)]
         #예약리스트 이차원 리스트
         self.reservationlist = [(member,userID,reservationcode,seats,cancel)
                                 for member,userID,reservationcode,seats,cancel in zip(RL.member,RL.userID,RL.reservationcode,RL.seats,RL.cancel)]
+        print(ML)
     def savefile(self):
         ##card
         df_cardlist = pd.DataFrame()
@@ -43,14 +44,21 @@ class FileManager:
         df_movielist.set_axis(["day","moviecode",'moviename','starttime','finishtime','screen','seat','A','B','C','D','E','F','G','H','I','j'],axis='columns',inplace=True)
         df_movielist.to_csv("movielist.csv",header=True,index=False)
 
+
     #아이디 존재 유무 리턴함수
 
-    #카드 번호드록 유무 리턴
-    #좌석 str 받으면 리스트 리턴
-    #회원 가입(아이디,비밀번호,카드 매개로 받아 user하나 더생성)
-    #영화예매(회원/비회원,아이디,영화 정보 ,,,좌석리스트) 받아 영화 예매
+    #카드 번호등록 유무 리턴 함수
+
+    #좌석 str 받으면 리스트 리턴 함수
+
+
+
+    #회원 가입(아이디,비밀번호,카드 매개로 받아 user하나 더생성)함수 //카드리스트랑 ,유저리스트 수정
+
+    #영화예매(회원/비회원,아이디,영화 정보 ,,,좌석리스트) 받아 영화 예매 함수, 유저리스트에서 마일리지, 예약리스트
 
 x =FileManager()
+# print(x.movielist)
 x.savefile()
 #사용예
 if 2<1:
