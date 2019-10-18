732c88c3f6ef79ce1506ced471557734963a81a5
leejaeseung <wasd222@naver.com>
2019-10-18 09:51:00 +0900

    영화시간표 외 모두 구현

--- main.py
+++ main.py
@@ -5,44 +5,27 @@ from Tickecter.TypeChecker import TypeChecker
 
 
 def main():
-    global input
     MM = MenuManager.MenuManager()
 
     while True:
-        input = input()
-        """TP = TypeChecker()
-        input2 = input()
-        TP.checkyoursheet(input2)
-        input2 = input()
-        TP.pw_check(input2)
-        input2 = input()
-        TP.ID_check(input2)
-        input2 = input()
-        TP.cardNum(input2)
-        input2 = input()
-        TP.date_check(input2)
-        input2 = input()
-        TP.movieTitle(input2)"""
-        if input == 'EXIT':
+        inpt = input()
+        if inpt == 'EXIT':
             break
-        elif input == 'RESTART':
+        elif inpt == 'RESTART':
             if MM.getMI().getwhere() != 0:
                 MM.getMI().setMI(4200, False, 1)
                 MM.userName = None
                 MM.password = None
                 os.system('cls')
                 MM.print_login_menu()
-            del (input)
             continue
-        elif input == 'BACK':
+        elif inpt == 'BACK':
             if MM.getMI().getwhere() == 2:
                 MM.getMI().setMI(4300, MM.getMI().getisMember(), MM.getMI().getwhere())
                 os.system('cls')
                 MM.print_main_menu()
-            del (input)
             continue
-        MM.manageMenu(input)
-        del (input)
+        MM.manageMenu(inpt)
 
     print("프로그램이 종료됩니다.")
 
